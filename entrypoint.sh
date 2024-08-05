9ee31cb2bd0ef2728a8646202ab412fc53f5138c
vinothsubramanian <vinothsubramanian@gmail.com>
2024-08-05 10:12:45 +0530

    Added code to handle Warp Connector (#26)
    * Added code to handle Warp Connector
    
    Signed-off-by: Ramasubramanian M <vinothsubramanian@gmail.com>
    
    * Fixed review comments
    
    Signed-off-by: Ramasubramanian M <vinothsubramanian@gmail.com>
    
    * Fix WARP case
    
    Signed-off-by: Ramasubramanian M <vinothsubramanian@gmail.com>
    
    ---------
    
    Signed-off-by: Ramasubramanian M <vinothsubramanian@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ sudo chmod 600 /dev/net/tun
 # start dbus
 sudo mkdir -p /run/dbus
 if [ -f /run/dbus/pid ]; then
-  sudo rm /run/dbus/pid
+    sudo rm /run/dbus/pid
 fi
 sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf
 
@@ -21,16 +21,19 @@ sudo warp-svc --accept-tos &
 # sleep to wait for the daemon to start, default 2 seconds
 sleep "$WARP_SLEEP"
 
-# if /var/lib/cloudflare-warp/reg.json not exists, register the warp client
+# if /var/lib/cloudflare-warp/reg.json not exists, setup new warp client
 if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
-    warp-cli registration new && echo "Warp client registered!"
-    # if a license key is provided, register the license
-    if [ -n "$WARP_LICENSE_KEY" ]; then
-        echo "License key found, registering license..."
-        warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
+    # if /var/lib/cloudflare-warp/mdm.xml not exists, register the warp client
+    if [ ! -f /var/lib/cloudflare-warp/mdm.xml ]; then
+        warp-cli registration new && echo "Warp client registered!"
+        # if a license key is provided, register the license
+        if [ -n "$WARP_LICENSE_KEY" ]; then
+            echo "License key found, registering license..."
+            warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
+        fi
     fi
     # connect to the warp server
-    warp-cli connect
+    warp-cli --accept-tos connect
 else
     echo "Warp client already registered, skip registration"
 fi
